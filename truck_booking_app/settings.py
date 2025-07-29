b0e884c8c60e371441141dd22d6e45485a33eaa3
Nuno Lopes <nunoslopesster@gmail.com>
2025-07-29 17:26:14 +0100

    1.16.3 - added windows for libre office

--- truck_booking_app/settings.py
+++ truck_booking_app/settings.py
@@ -190,7 +190,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-APP_VERSION = 'v0.12.2'
+APP_VERSION = 'v0.12.3'
 APP_UPDATE_DATE = date(2025, 7, 29)
 
 IMPERSONATE_PERMISSIONS = 'impersonate.permissions.staff'
