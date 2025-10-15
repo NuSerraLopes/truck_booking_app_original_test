5b734b0fcd2251fb869e212b998062b41008c3be
Nuno Lopes <nunoslopesster@gmail.com>
2025-10-15 15:16:57 +0100

    2.2.2 - corrected errors on Email notification

--- truck_booking_app/settings.py
+++ truck_booking_app/settings.py
@@ -194,7 +194,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-APP_VERSION = 'v0.17.3'
+APP_VERSION = 'v0.17.4'
 APP_UPDATE_DATE = date(2025, 10, 15)
 
 IMPERSONATE_PERMISSIONS = 'impersonate.permissions.staff'
